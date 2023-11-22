ebc061ce884d4882cbf43635ceedeb6ce2efcf3c
Cole Peters <cole@colepeters.com>
2023-11-22 13:14:54 -0600

    Hyphenate

--- app/elements/site-header.mjs
+++ app/elements/site-header.mjs
@@ -6,7 +6,7 @@ export default function SiteMasthead ({ html }) {
         --max-inline-size: var(--site-max-width);
       }
     </style>
-    <begin-masthead product="awslite" breakpoint="52em" active="products">
+    <begin-masthead product="aws-lite" breakpoint="52em" active="products">
       <span slot="product-page">Docs</span>
       <site-nav slot="product-nav"></site-nav>
       <div slot="product-nav-lg" class="flex align-items-center">
