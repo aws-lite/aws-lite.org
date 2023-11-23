0e63705f2cde50e738227dd4bd790c90c74e48ba
Ryan Block <rblock@gmail.com>
2023-11-23 10:54:51 -0800

    Add method examples Sort properties by required, then alphabetically

--- app/docs/services/generate-methods.mjs
+++ app/docs/services/generate-methods.mjs
@@ -1,3 +1,15 @@
+const fenceStart = '```javascript'
+const fenceEnd = '```'
+const reqDoc = ' [required]'
+const reqComment = ' // required'
+const types = {
+  array: 'Array',
+  boolean: 'Boolean',
+  number: 'Number',
+  object: 'Object',
+  string: 'String',
+}
+
 export default function generateMethods (_plugin) {
   const { display } = _plugin
 
@@ -24,12 +36,20 @@ export default function generateMethods (_plugin) {
       header += `\n #### Properties\n` + '<dl>' + Object.entries(validate).map(([ param, values ]) => {
         const { type, required, comment } = values
         const _typ = Array.isArray(type) ? type.join(', ') : type
-        const _req = required ? ' [required]' : ''
+        const _req = required ? reqDoc : ''
         // Newlines required in `dd` to preserve any markdown formatting contained in the comment
         const _com = comment ? `<dd>\n\n${comment}\n\n</dd>` : ''
         return `<dt><code>${param}</code> (${_typ})${_req}</dt>${_com}`
-      }).join('\n') + '</dl>'
+      })
+        .sort((a, b) => {
+          if (a.includes(reqDoc) && b.includes(reqDoc)) return a > b ? 1 : -1
+          if (a.includes(reqDoc)) return -1
+          if (b.includes(reqDoc)) return 1
+          return a > b ? 1 : -1
+        })
+        .join('\n') + '</dl>'
     }
+    header += '\n\n' + getExample(_plugin, method)
     header += '\n\n</figure>'
     return header
   }).filter(Boolean).join('\n\n\n') + '\n'
@@ -50,4 +70,38 @@ export default function generateMethods (_plugin) {
                   ).join('\n') + '\n'
   }
   return methodDocs
+}
+
+function getExample (_plugin, method) {
+  const v = _plugin.methods[method].validate
+  let params = ''
+  if (Object.keys(v).length) {
+    const props = Object.entries(v)
+      .reduce((acc, [ prop, deets ]) => {
+        const type = Array.isArray(deets.type)
+          ? deets.type.map(d => types[d]).join(' || ')
+          : types[deets.type]
+        let item = `  ${prop}: ${type},`
+        if (deets.required) item += reqComment
+        acc.push(item)
+        return acc
+      }, [])
+      .sort((a, b) => {
+        if (a.endsWith(reqComment) && b.endsWith(reqComment)) return a > b ? 1 : -1
+        if (a.endsWith(reqComment)) return -1
+        if (b.endsWith(reqComment)) return 1
+        return a > b ? 1 : -1
+      })
+      .join('\n')
+    params = '{\n' + props + '\n}'
+  }
+  const code = `await aws.${_plugin.property}.${method}(${params})`
+
+  const example = [
+    '#### Example',
+    fenceStart,
+    code,
+    fenceEnd,
+  ]
+  return example.join('\n\n')
 }
